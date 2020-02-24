3b806d91c7cb8df8674e7db0946842623d67f078
Bernal <berrojas45@gmail.com>
2020-02-24 08:30:01 -0600

    Connect backend. IP, Score and Category. TODO: Country

--- html/js/main.js
+++ html/js/main.js
@@ -4,24 +4,37 @@ function request() {
       return response.json();
     })
     .then(function(myJson) {
+      console.log(myJson);
+      cleanCategories();
       document.getElementById("id_score").innerHTML = myJson.score;
       document.getElementById("id_ip").innerHTML = myJson.ip;
       changeColorCategories(myJson.bots, myJson.crypto_mining, myJson.ip_scan,myJson.ip_dynamic,myJson.malware,myJson.anonymization,myJson.botnets_command_center,myJson.spam);
     });
 }
 
-function changeColorCategories(bots, crypto_mining, ip_scan, ip_dynamic, malware, anonymization, botnets_command_center, spam) {
-if (bots === true){document.getElementById("id_bots").style.backgroundColor = "#bd1111";}
-if (crypto_mining === true){document.getElementById("id_crypto_mining").style.backgroundColor = "#bd1111";}
-if (ip_scan === true){document.getElementById("id_ip_scan").style.backgroundColor = "#bd1111";}
-if (ip_dynamic === true){document.getElementById("id_ip_dynamic").style.backgroundColor = "#bd1111";}
-if (malware === true){document.getElementById("id_malware").style.backgroundColor = "#bd1111";}
-if (anonymization === true){document.getElementById("id_anonymization").style.backgroundColor = "#bd1111";}
-if (botnets_command_center === true){document.getElementById("id_botnets_command_center").style.backgroundColor = "#bd1111";}
-if (spam === true){document.getElementById("id_spam").style.backgroundColor = "#bd1111";}
+function cleanCategories(){
+  document.getElementById("id_score").innerHTML = "";
+  document.getElementById("id_ip").innerHTML = "";
+  document.getElementById("id_bots").style.backgroundColor = "rgba(189, 17, 17, 0.445)";
+  document.getElementById("id_crypto_mining").style.backgroundColor = "rgba(189, 17, 17, 0.445)";
+  document.getElementById("id_ip_scan").style.backgroundColor = "rgba(189, 17, 17, 0.445)";
+  document.getElementById("id_ip_dynamic").style.backgroundColor = "rgba(189, 17, 17, 0.445)";
+  document.getElementById("id_malware").style.backgroundColor = "rgba(189, 17, 17, 0.445)";
+  document.getElementById("id_anonymization").style.backgroundColor = "rgba(189, 17, 17, 0.445)";
+  document.getElementById("id_botnets_command_center").style.backgroundColor = "rgba(189, 17, 17, 0.445)";
+  document.getElementById("id_spam").style.backgroundColor = "rgba(189, 17, 17, 0.445)";
 }
 
-
+function changeColorCategories(bots, crypto_mining, ip_scan, ip_dynamic, malware, anonymization, botnets_command_center, spam) {
+if (bots == true){document.getElementById("id_bots").style.backgroundColor = "#bd1111";}
+if (crypto_mining == true){document.getElementById("id_crypto_mining").style.backgroundColor = "#bd1111";}
+if (ip_scan == true){document.getElementById("id_ip_scan").style.backgroundColor = "#bd1111";}
+if (ip_dynamic == true){document.getElementById("id_ip_dynamic").style.backgroundColor = "#bd1111";}
+if (malware == true){document.getElementById("id_malware").style.backgroundColor = "#bd1111";}
+if (anonymization == true){document.getElementById("id_anonymization").style.backgroundColor = "#bd1111";}
+if (botnets_command_center == true){document.getElementById("id_botnets_command_center").style.backgroundColor = "#bd1111";}
+if (spam == true){document.getElementById("id_spam").style.backgroundColor = "#bd1111";}
+}
 
 jQuery(document).ready(function () {
 jQuery('#vmap').vectorMap({
