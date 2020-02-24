e4d5be927ee5b91b6ded4897dcba40aea7379374
Bernal <berrojas45@gmail.com>
2020-02-24 15:33:12 -0600

    TODO: Sincronize db replicas

--- api/src/app/main.py
+++ api/src/app/main.py
@@ -3,7 +3,7 @@ from .routers import analysis_metrics
 from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI(
-    title='API Microservice 2',
+    title='API Microservice',
     description='A Python-based microservice that expose an web interface to capture ip analysis metrics', 
     version='1.0.0', 
     openapi_url='/api/openapi.json',
@@ -15,7 +15,7 @@ app.include_router(analysis_metrics.router, prefix='/api/v1')
 
 origins = [
     "http://localhost:31000",
-    "http://localhost:8081",
+    "http://localhost:8081"
 ]
 
 app.add_middleware(
--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
 
-cd api/
-sh build-api.sh
-cd ../
-
-cd db/
-sh build-db.sh
-cd ../
-
-cd html/
-sh build-ui.sh
-cd ../
-
 kubectl apply -f kube-deployment.yml
